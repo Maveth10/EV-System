1f1fb7f94a9462940324f20c0c959a88c17e0e71
Maveth10 <sagajdakow@gmail.com>
2026-05-19 14:15:57 +0200

    1

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://tkeigbxkmvctjryijhgw.supabase.co';
+const supabaseKey = 'sb_publishable_ZWfUDMzpfOt9nk5i07aFVA_0VMpxsLr';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

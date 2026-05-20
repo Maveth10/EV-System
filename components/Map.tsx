082bf78a3c5223d6d13e1d15be5da4365f5e7f11
Maveth10 <sagajdakow@gmail.com>
2026-05-20 20:31:21 +0200

    1a

--- components/Map.tsx
+++ components/Map.tsx
@@ -19,7 +19,9 @@ import { LoadingScreen } from './EkoenLogo';
 
 import { buildOuterBoundary, mergeRegions, clipToBoundary, ensureMultiPolygon } from '../utils/geometryEngine';
 
-const DETAILED_POLAND_URL = 'https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/wojewodztwa/wojewodztwa-medium.geojson';
+//const DETAILED_POLAND_URL = 'https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/wojewodztwa/wojewodztwa-medium.geojson';
+const DETAILED_POLAND_URL = '/poland.geojson';
+const DETAILED_SLOVAKIA_URL = '/slovakia.geojson';
 
 const IconFilter = () => <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"/></svg>;
 
